d7e5fe1f46a051a206e2872427f7aecffd4405b3
DANTE LUIS ENRIQUE ALVAREZ ARRIAGADA <d.alvarez12@ufromail.cl>
2021-01-25 19:16:46 -0300

    Dante sube el programa funcionando, faltan 2 detalles

---
+++ Programa Funcional Faltan 2 detalles/Prueba.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Prueba
+{
+    public static void main(String[] args) {
+        // GestorArchivos gestorArchivos = new GestorArchivos();
+        //gestorArchivos.crearDirectorio();
+        // gestorArchivos.crearArchivo("Jueves","hola que tal");
+        //gestorArchivos.contarLineas("poto");
+        //gestorArchivos.eliminarArchivos("Lunes");
+        JOptionPane.showConfirmDialog(null,"Misión creada con exito","Éxito!!", JOptionPane.DEFAULT_OPTION);
+        String hola = null;
+        //int numero=Integer.parseInt(hola);
+        
+    }
+}
